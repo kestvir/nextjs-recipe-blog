cddd055b4cf54d85a2dd65ca2a72e12d050fa6ce
kestutisvirbickas <v.kestutis123@gmail.com>
2020-11-21 19:58:16 +0200

    testing

--- pages/categories/courses/[course].tsx
+++ pages/categories/courses/[course].tsx
@@ -4,14 +4,25 @@ import Prismic from "prismic-javascript";
 import { useRouter } from "next/router";
 import { Client } from "../../../prismic-configuration";
 import Sidebar from "../../../components/Sidebar";
+import ApiSearchResponse from "prismic-javascript/types/ApiSearchResponse";
 import { Document as PrismicDoc } from "prismic-javascript/types/documents";
 import RecipeListItem from "../../../components/RecipeListItem";
+
+export interface CourseRecipesProps {
+  postsData: ApiSearchResponse;
+  tag: string;
+}
 
 export async function getStaticProps({ params }) {
   const client = Client();
   //   params.course.charAt(0).toUpperCase() + params.course.slice(1);
-  const postsData =
-    (await Client().getByUID("blog_post", params.course, {})) || {};
+  const postsData = await client.query(
+    Prismic.Predicates.at("document.tags", [params.course]),
+    {
+      orderings: "[my.blog_post.first_publication_date desc]",
+      pageSize: 12,
+    }
+  );
   return {
     props: {
       postsData,
@@ -29,11 +40,6 @@ export async function getStaticPaths() {
   };
 }
 
-export interface CourseRecipesProps {
-  postsData: PrismicDoc;
-  tag: string;
-}
-
 const CourseRecipes: React.FC<CourseRecipesProps> = ({ postsData }) => {
   const router = useRouter();
 
@@ -48,7 +54,9 @@ const CourseRecipes: React.FC<CourseRecipesProps> = ({ postsData }) => {
     <div className="course">
       <div className="gridWithSidebar">
         <div className="course-inner">
-          <h2>{postsData.data.title[0].text}</h2>
+          <h2>{postsData.page}</h2>
+
+          {/* <h2>{postsData.data.title[0].text}</h2> */}
 
           {/* <h2 className="course__title">{tag} dishes: </h2>
           <ul className="grid3">
