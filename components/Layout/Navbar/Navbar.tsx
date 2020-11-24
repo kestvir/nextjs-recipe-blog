538b43f946a153bbee26cef23fe960d0cbcdfbff
kestutisvirbickas <v.kestutis123@gmail.com>
2020-11-24 12:16:56 +0200

    removed animations from mobile menu

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import SearchBar from "../../SearchBar";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavLinks from "./NavLinks";
-import MobileNav from "./MobileNav";
 
 export interface NavbarProps {
   displayMobileNav: () => void;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ displayMobileNav }) => {
-  // const [displayMobileNav, setDisplayMobileNav] = useState(false);
-
   return (
     <nav className="nav">
       <div className="container">
@@ -21,17 +17,6 @@ const Navbar: React.FC<NavbarProps> = ({ displayMobileNav }) => {
             className="nav__menuIcon"
             icon={faBars}
           />
-
-          {/* <FontAwesomeIcon
-            onClick={() => setDisplayMobileNav(true)}
-            className="nav__menuIcon"
-            icon={faBars}
-          /> */}
-
-          {/* <MobileNav
-            isOpen={displayMobileNav}
-            closeMobileNav={() => setDisplayMobileNav(false)}
-          /> */}
 
           <ul className="nav__linksList">
             <NavLinks linkClassName="nav__link" />
