0354f813443011031f973a038b967ba50765f5f1
kestutisvirbickas <v.kestutis123@gmail.com>
2020-11-27 13:56:48 +0200

    footer component import

--- styled/layout/Footer.tsx
+++ styled/layout/Footer.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-  /* display: flex;
+  display: flex;
   justify-content: center;
   border-top: 1px solid #eee;
   padding: 0.8rem 0;
 
   @media screen and (max-width: 358px) {
     font-size: 0.8rem;
-  } */
+  }
 `;
